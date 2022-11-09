9e9d02917ecadc06211bde44e2165d951a4796c9
yash-joshi-vineti <yash.joshi@vineti.com>
2022-11-09 23:30:27 +0530

    Add Navbar styled-component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,20 @@
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
-import styled from "styled-components";
+import {
+	Container,
+	Wrapper,
+	Left,
+	Language,
+	SearchContainer,
+	Input,
+	Center,
+	Logo,
+	Right,
+	MenuItem,
+} from "./utils/styledComponents/Navbar";
 
-const Container = styled.div`
-	height: 60px;
-`;
 
-const Wrapper = styled.div`
-	padding: 10px 20px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-`;
-
-const Left = styled.div`
-	flex: 1;
-	display: flex;
-	align-items: center;
-`;
-const Center = styled.div`
-	flex: 1;
-  text-align: center;
-`;
-const Right = styled.div`
-	flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`;
-
-const Language = styled.span`
-	font-size: 14px;
-	cursor: pointer;
-`;
-const SearchContainer = styled.div`
-	border: 1px solid lightgray;
-	display: flex;
-	align-items: center;
-	margin-left: 25px;
-	padding: 5px;
-`;
-
-const Input = styled.input`
-	border: none;
-`;
-
-const Logo = styled.h1`
-	font-weight: bold;
-`;
-
-const MenuItem = styled.div`
-  font-size: 14px;
-  cursor: pointer;
-  margin-left: 25px;
-`
 const Navbar = () => {
 	return (
 		<Container>
