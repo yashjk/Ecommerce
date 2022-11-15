53281753c7cfca8784acc2f364ee4153ff05d71d
yash-joshi-vineti <yash.joshi@vineti.com>
2022-11-16 01:02:30 +0530

    Add dummy categories

--- src/data.js
+++ src/data.js
@@ -21,3 +21,21 @@ export const sliderItems = [
 		bg: "fbf0f4",
 	},
 ];
+
+export const categories = [
+	{
+		id: 1,
+		img: "https://images.pexels.com/photos/1254502/pexels-photo-1254502.jpeg?cs=srgb&dl=pexels-ali-madad-sakhirani-1254502.jpg&fm=jpg",
+		title: "SHIRT STYLE",
+	},
+	{
+		id: 2,
+		img: "https://s3-eu-west-1.amazonaws.com/emap-nibiru-prod/wp-content/uploads/sites/2/2020/11/02173649/Modern-Rarity-Cashmere-768x1024.jpg",
+		title: "LOUNGEWEAR LOVE",
+	},
+	{
+		id: 3,
+		img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/15131036/2022/3/24/d5566209-3e53-46a0-9946-505cbd8786d41648100796044-WROGN-Men-Olive-Green-Sporty-Jacket-7591648100795523-1.jpg",
+		title: "LIGHT JACKETS",
+	},
+];
