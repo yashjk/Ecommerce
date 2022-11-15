f9be8807d086bbe13b1fa0235d581e63dbde6e90
yash-joshi-vineti <yash.joshi@vineti.com>
2022-11-15 23:48:26 +0530

    Add dummy data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+	{
+		id: 1,
+		title: "SUMMER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+		img: "https://media.glamour.com/photos/5695872c5fff94d44eec6931/master/w_1500,h_2250,c_limit/fashion-2014-05-07-white-main.jpg",
+		bg: "f5fafd",
+	},
+	{
+		id: 2,
+		title: "WINTER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+		img: "https://i.pinimg.com/736x/0a/2a/6e/0a2a6e3134d03e45278911384cb16265.jpg",
+		bg: "fcf1ed",
+	},
+	{
+		id: 3,
+		title: "SPRING SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+		img: "https://www.itakeyou.co.uk/wp-content/uploads/2020/02/spring-outfit-ideas.jpg",
+		bg: "fbf0f4",
+	},
+];
