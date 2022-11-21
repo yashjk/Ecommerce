9950c1d8fa2d2b577fa2cf7dfc653612a8bd37e5
yash-joshi-vineti <yash.joshi@vineti.com>
2022-11-21 16:56:07 +0530

    Add footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+import {
+	Facebook,
+	Instagram,
+	MailOutline,
+	Phone,
+	Room,
+	Telegram,
+	Twitter,
+} from "@material-ui/icons";
+
+import {
+	Center,
+	ContactItem,
+	Container,
+	Desc,
+	IconStyle,
+	Left,
+	List,
+	ListItem,
+	Logo,
+	Payment,
+	Right,
+	SocialContainer,
+	SocialIcon,
+	Title,
+} from "./utils/styledComponents/Footer";
+
+const Footer = () => {
+	return (
+		<Container>
+			<Left>
+				<Logo>SPODER.</Logo>
+				<Desc>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Exercitationem cupiditate harum nostrum ea optio quod dignissimos,
+					reiciendis magnam aliquam error dolore a molestiae nulla sequi commodi
+					maxime quidem corrupti labore.
+				</Desc>
+				<SocialContainer>
+					<SocialIcon color="3b5998">
+						<Facebook />
+					</SocialIcon>
+					<SocialIcon color="00acee">
+						<Twitter />
+					</SocialIcon>
+					<SocialIcon color="bc2a8d">
+						<Instagram />
+					</SocialIcon>
+					<SocialIcon color="0088cc">
+						<Telegram />
+					</SocialIcon>
+				</SocialContainer>
+			</Left>
+			<Center>
+				<Title>Useful links</Title>
+				<List>
+					<ListItem>Home</ListItem>
+					<ListItem>Cart</ListItem>
+					<ListItem>Men's Fashion</ListItem>
+					<ListItem>Women's Fashion</ListItem>
+					<ListItem>Accessories</ListItem>
+					<ListItem>My Account</ListItem>
+					<ListItem>Order Tracking</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Terms</ListItem>
+				</List>
+			</Center>
+			<Right>
+				<Title>Contact</Title>
+				<ContactItem>
+					<Room style={{marginRight: "10px"}}/>
+					London N1 9AP, United Kingdom
+				</ContactItem>
+				<ContactItem>
+					<Phone style={{marginRight: "10px"}}/>
+					+44 20 7833 1137
+				</ContactItem>
+				<ContactItem>
+					<MailOutline style={{marginRight: "10px"}}/>
+					contact@spoder.dev
+				</ContactItem>
+				<Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+			</Right>
+		</Container>
+	);
+};
+
+export default Footer;
